8d657333d06d1ed43a88679f26d516e76a9e29aa
janegl2 <jgutierrezlo@unal.edu.co>
2021-11-23 20:13:52 -0500

    :''''''''''(

---
+++ src/main/java/zooAnimales/Mamifero.java
@@ -0,0 +1,82 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+import gestion.Zona;
+
+public class Mamifero extends Animal{
+	private static ArrayList<Mamifero> listado = new ArrayList<Mamifero>();
+	public static int caballos = 0;
+	public static int leones = 0;
+	private boolean pelaje;
+	private int patas;
+	
+	public Mamifero(String nombre, int edad, String habitat, String genero, Zona zonas, boolean pelaje, int patas) {
+		totalAnimales++;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.habitat = habitat;
+		this.genero = genero;
+		this.zonas = zonas;
+		
+		listado.add(this);
+		this.pelaje = pelaje;
+		this.patas = patas;
+	}
+	
+	public Mamifero() {
+		totalAnimales++;
+		listado.add(this);
+	}
+	
+	public static int cantidadMamiferos() {return listado.size();}
+	
+	public static Mamifero crearCaballo(String nombre, int edad, String genero) {
+		caballos++;
+		return new Mamifero(nombre, edad, "pradera", genero, null, true, 4);
+	}
+	
+	public static Mamifero crearLeon(String nombre, int edad, String genero) {
+		leones++;
+		return new Mamifero(nombre, edad, "selva", genero, null, true, 4);
+	}
+
+	public static ArrayList<Mamifero> getListado() {
+		return listado;
+	}
+
+	public static void setListado(ArrayList<Mamifero> listado) {
+		Mamifero.listado = listado;
+	}
+
+	public static int getCaballos() {
+		return caballos;
+	}
+
+	public static void setCaballos(int caballos) {
+		Mamifero.caballos = caballos;
+	}
+
+	public static int getLeones() {
+		return leones;
+	}
+
+	public static void setLeones(int leones) {
+		Mamifero.leones = leones;
+	}
+
+	public boolean isPelaje() {
+		return pelaje;
+	}
+
+	public void setPelaje(boolean pelaje) {
+		this.pelaje = pelaje;
+	}
+
+	public int getPatas() {
+		return patas;
+	}
+
+	public void setPatas(int patas) {
+		this.patas = patas;
+	}
+}
---
+++ src/main/java/zooAnimales/Reptil.java
@@ -0,0 +1,85 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+import gestion.Zona;
+
+public class Reptil extends Animal {
+	private static ArrayList<Reptil> listado = new ArrayList<Reptil>();
+	public static int iguanas = 0;
+	public static int serpientes = 0;
+	private String colorEscamas;
+	private int largoCola;
+	
+	public Reptil(String nombre, int edad, String habitat, String genero, Zona zonas, String colorEscamas, int largoCola) {
+		totalAnimales++;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.habitat = habitat;
+		this.genero = genero;
+		this.zonas = zonas;
+		
+		listado.add(this);
+		this.colorEscamas = colorEscamas;
+		this.largoCola = largoCola;
+	}
+	
+	public Reptil() {
+		totalAnimales++;
+		listado.add(this);
+	}
+	
+	public static int cantidadReptiles() {return listado.size();}
+	
+	@Override
+	public String movimiento() {return "reptar";}
+	
+	public static Reptil crearIguana(String nombre, int edad, String genero) {
+		iguanas++;
+		return new Reptil(nombre, edad, "humedal", genero, null, "verdes", 3);
+	}
+	
+	public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+		serpientes++;
+		return new Reptil(nombre, edad, "jungla", genero, null, "blanco", 1);
+	}
+
+	public static ArrayList<Reptil> getListado() {
+		return listado;
+	}
+
+	public static void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado = listado;
+	}
+
+	public static int getIguanas() {
+		return iguanas;
+	}
+
+	public static void setIguanas(int iguanas) {
+		Reptil.iguanas = iguanas;
+	}
+
+	public static int getSerpientes() {
+		return serpientes;
+	}
+
+	public static void setSerpientes(int serpientes) {
+		Reptil.serpientes = serpientes;
+	}
+
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+
+	public int getLargoCola() {
+		return largoCola;
+	}
+
+	public void setLargoCola(int largoCola) {
+		this.largoCola = largoCola;
+	}
+}
